822674b6de6282a74719df7e4e8ca2891f55ed6e
Danylo Nepochatov <danylonepochatov@Danylos-MacBook-Pro.local>
2025-07-14 16:32:51 +0300

    Add calculator

---
+++ app.py
@@ -0,0 +1,13 @@
+x = float(input("Enter the first number: "))
+operation = input("Enter operation (+ or -): ")
+y = float(input("Enter the second number: "))
+
+if operation == "+":
+    result = x + y
+elif operation == "-":
+    result = x - y
+else:
+    print("Unknown operation!")
+    exit()
+
+print("Result:", result)
